2314dd93c460ba302152d0fd5a32a1a42d689ff3
Csnyder0920 <33033510+Csnyder0920@users.noreply.github.com>
2017-11-21 11:01:57 -0600

    Added RomanNumeralConverterProgram

---
+++ RomanNumeralConverterProgram/RomanNumeralConverterProgram/Form1.Designer.vb
@@ -0,0 +1,133 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.lblResult = New System.Windows.Forms.Label()
+        Me.txtInteger = New System.Windows.Forms.TextBox()
+        Me.btnConvert = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.lblErrorMessage = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(38, 22)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(115, 26)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Enter a decimal integer" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "in the range 1-10:"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.BottomRight
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(59, 64)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(94, 26)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Equivalent Roman" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Numeral:"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'lblResult
+        '
+        Me.lblResult.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblResult.Location = New System.Drawing.Point(159, 64)
+        Me.lblResult.Name = "lblResult"
+        Me.lblResult.Size = New System.Drawing.Size(150, 20)
+        Me.lblResult.TabIndex = 2
+        '
+        'txtInteger
+        '
+        Me.txtInteger.Location = New System.Drawing.Point(159, 22)
+        Me.txtInteger.Name = "txtInteger"
+        Me.txtInteger.Size = New System.Drawing.Size(150, 20)
+        Me.txtInteger.TabIndex = 3
+        '
+        'btnConvert
+        '
+        Me.btnConvert.Location = New System.Drawing.Point(50, 138)
+        Me.btnConvert.Name = "btnConvert"
+        Me.btnConvert.Size = New System.Drawing.Size(71, 35)
+        Me.btnConvert.TabIndex = 4
+        Me.btnConvert.Text = "Convert"
+        Me.btnConvert.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(238, 138)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(71, 35)
+        Me.btnExit.TabIndex = 5
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(142, 138)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(71, 35)
+        Me.btnClear.TabIndex = 6
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'lblErrorMessage
+        '
+        Me.lblErrorMessage.AutoSize = True
+        Me.lblErrorMessage.Location = New System.Drawing.Point(84, 100)
+        Me.lblErrorMessage.Name = "lblErrorMessage"
+        Me.lblErrorMessage.Size = New System.Drawing.Size(0, 13)
+        Me.lblErrorMessage.TabIndex = 7
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(373, 185)
+        Me.Controls.Add(Me.lblErrorMessage)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnConvert)
+        Me.Controls.Add(Me.txtInteger)
+        Me.Controls.Add(Me.lblResult)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Roman Numeral Converter"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents lblResult As System.Windows.Forms.Label
+    Friend WithEvents txtInteger As System.Windows.Forms.TextBox
+    Friend WithEvents btnConvert As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+    Friend WithEvents btnClear As System.Windows.Forms.Button
+    Friend WithEvents lblErrorMessage As System.Windows.Forms.Label
+
+End Class
---
+++ RomanNumeralConverterProgram/RomanNumeralConverterProgram/Form1.vb
@@ -0,0 +1,57 @@
+﻿Public Class Form1
+
+    Private Sub btnConvert_Click(sender As Object, e As EventArgs) Handles btnConvert.Click
+        Dim intError As Integer
+        Dim intRomanNumeral As Integer
+        Try
+            If txtInteger.Text = 1 Then
+                lblResult.Text = "I"
+            End If
+            If txtInteger.Text = 2 Then
+                lblResult.Text = "II"
+            End If
+            If txtInteger.Text = 3 Then
+                lblResult.Text = "III"
+            End If
+            If txtInteger.Text = 4 Then
+                lblResult.Text = "IV"
+            End If
+            If txtInteger.Text = 5 Then
+                lblResult.Text = "V"
+            End If
+            If txtInteger.Text = 6 Then
+                lblResult.Text = "VI"
+            End If
+            If txtInteger.Text = 7 Then
+                lblResult.Text = "VII"
+            End If
+            If txtInteger.Text = 8 Then
+                lblResult.Text = "VIII"
+            End If
+            If txtInteger.Text = 9 Then
+                lblResult.Text = "IX"
+            End If
+            If txtInteger.Text = 10 Then
+                lblResult.Text = "X"
+            End If
+        Catch
+            intRomanNumeral = CInt(txtInteger.Text)
+            intError = CInt(txtInteger.Text)
+
+            If intRomanNumeral > 1 Or intRomanNumeral < 10 Then
+                lblErrorMessage.Text = "Integer entered must be between 1 and 10!!!"
+            End If
+        End Try
+
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        lblResult.Text = ""
+        lblErrorMessage.Text = ""
+        txtInteger.Clear()
+    End Sub
+End Class
