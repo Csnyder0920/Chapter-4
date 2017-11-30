554a49f290c02291566ec513580f55c03ce702b3
Csnyder0920 <33033510+Csnyder0920@users.noreply.github.com>
2017-11-30 10:27:22 -0600

    Added HelloWorldProgram

--- HelloWorld/HelloWorld/Form1.Designer.vb
+++ HelloWorld/HelloWorld/Form1.Designer.vb
@@ -22,21 +22,22 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.txtPassword = New System.Windows.Forms.TextBox()
         Me.btnEnter = New System.Windows.Forms.Button()
         Me.btnExit = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
-        'TextBox1
+        'txtPassword
         '
-        Me.TextBox1.Location = New System.Drawing.Point(67, 41)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(146, 20)
-        Me.TextBox1.TabIndex = 0
+        Me.txtPassword.Location = New System.Drawing.Point(67, 41)
+        Me.txtPassword.Name = "txtPassword"
+        Me.txtPassword.Size = New System.Drawing.Size(146, 20)
+        Me.txtPassword.TabIndex = 0
         '
         'btnEnter
         '
-        Me.btnEnter.Location = New System.Drawing.Point(47, 89)
+        Me.btnEnter.Location = New System.Drawing.Point(12, 89)
         Me.btnEnter.Name = "btnEnter"
         Me.btnEnter.Size = New System.Drawing.Size(75, 29)
         Me.btnEnter.TabIndex = 1
@@ -45,7 +46,7 @@ Partial Class Form1
         '
         'btnExit
         '
-        Me.btnExit.Location = New System.Drawing.Point(150, 89)
+        Me.btnExit.Location = New System.Drawing.Point(197, 89)
         Me.btnExit.Name = "btnExit"
         Me.btnExit.RightToLeft = System.Windows.Forms.RightToLeft.No
         Me.btnExit.Size = New System.Drawing.Size(75, 29)
@@ -53,22 +54,33 @@ Partial Class Form1
         Me.btnExit.Text = "Exit"
         Me.btnExit.UseVisualStyleBackColor = True
         '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(104, 89)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(75, 29)
+        Me.btnClear.TabIndex = 3
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(284, 143)
+        Me.Controls.Add(Me.btnClear)
         Me.Controls.Add(Me.btnExit)
         Me.Controls.Add(Me.btnEnter)
-        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.txtPassword)
         Me.Name = "Form1"
         Me.Text = "Hello World Password"
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
-    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents txtPassword As System.Windows.Forms.TextBox
     Friend WithEvents btnEnter As System.Windows.Forms.Button
     Friend WithEvents btnExit As System.Windows.Forms.Button
+    Friend WithEvents btnClear As System.Windows.Forms.Button
 
 End Class
--- HelloWorld/HelloWorld/Form1.vb
+++ HelloWorld/HelloWorld/Form1.vb
@@ -2,6 +2,18 @@
 
  
     Private Sub btnEnter_Click(sender As Object, e As EventArgs) Handles btnEnter.Click
-        Dim 
+        If txtPassword.Text = "Go Cubs Go!" Then
+            MessageBox.Show("Hello World!")
+        Else
+            MessageBox.Show("Try Again")
+        End If
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        txtPassword.Text = ""
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
     End Sub
 End Class
